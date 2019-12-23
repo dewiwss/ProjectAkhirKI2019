d5ec675ab188ed5e9dd284512a8e822adba6facd
Dewi Wulan Sari <dewiws27@gmail.com>
2019-12-23 18:03:22 +0700

    push stegano.py dan rc4

---
+++ compareLSBXDCT/stegano.py
@@ -0,0 +1,489 @@
+#!/usr/bin/python
+import os
+import xlwt
+import shutil
+import cv2
+import sys
+import math
+import time
+import numpy as np
+import itertools
+import matplotlib.pyplot as plt
+from PIL import Image
+from pathlib import Path
+from scipy import signal
+# crypto
+import codecs
+
+# crypto rc4
+MOD = 256
+
+
+def KSA(key):
+    ''' Key Scheduling Algorithm (from wikipedia):
+        for i from 0 to 255
+            S[i] := i
+        endfor
+        j := 0
+        for i from 0 to 255
+            j := (j + S[i] + key[i mod keylength]) mod 256
+            swap values of S[i] and S[j]
+        endfor
+    '''
+    key_length = len(key)
+    # create the array "S"
+    S = list(range(MOD))  # [0,1,2, ... , 255]
+    j = 0
+    for i in range(MOD):
+        j = (j + S[i] + key[i % key_length]) % MOD
+        S[i], S[j] = S[j], S[i]  # swap values
+
+    return S
+
+
+def PRGA(S):
+    ''' Psudo Random Generation Algorithm (from wikipedia):
+        i := 0
+        j := 0
+        while GeneratingOutput:
+            i := (i + 1) mod 256
+            j := (j + S[i]) mod 256
+            swap values of S[i] and S[j]
+            K := S[(S[i] + S[j]) mod 256]
+            output K
+        endwhile
+    '''
+    i = 0
+    j = 0
+    while True:
+        i = (i + 1) % MOD
+        j = (j + S[i]) % MOD
+
+        S[i], S[j] = S[j], S[i]  # swap values
+        K = S[(S[i] + S[j]) % MOD]
+        yield K
+
+
+def get_keystream(key):
+    ''' Takes the encryption key to get the keystream using PRGA
+        return object is a generator
+    '''
+    S = KSA(key)
+    return PRGA(S)
+
+
+def encrypt_logic(key, text):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :text -> array of unicode values/ byte string to encrpyt/decrypt
+    '''
+    # For plaintext key, use this
+    key = [ord(c) for c in key]
+    # If key is in hex:
+    # key = codecs.decode(key, 'hex_codec')
+    # key = [c for c in key]
+    keystream = get_keystream(key)
+
+    res = []
+    for c in text:
+        val = ("%02X" % (c ^ next(keystream)))  # XOR and taking hex
+        res.append(val)
+    return ''.join(res)
+
+
+def encrypt(key, plaintext):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :plaintext -> plaintext string to encrpyt
+    '''
+    plaintext = [ord(c) for c in plaintext]
+    return encrypt_logic(key, plaintext)
+
+
+def decrypt(key, ciphertext):
+    ''' :key -> encryption key used for encrypting, as hex string
+        :ciphertext -> hex encoded ciphered text using RC4
+    '''
+    ciphertext = codecs.decode(ciphertext, 'hex_codec')
+    res = encrypt_logic(key, ciphertext)
+    return codecs.decode(res, 'hex_codec').decode('utf-8')
+
+
+
+# steganography
+quant = np.array([[16,11,10,16,24,40,51,61],      # QUANTIZATION TABLE
+                    [12,12,14,19,26,58,60,55],    # required for DCT
+                    [14,13,16,24,40,57,69,56],
+                    [14,17,22,29,51,87,80,62],
+                    [18,22,37,56,68,109,103,77],
+                    [24,35,55,64,81,104,113,92],
+                    [49,64,78,87,103,121,120,101],
+                    [72,92,95,98,112,100,103,99]])
+def show(im):
+    im_resized = cv2.resize(im, (500, 500), interpolation=cv2.INTER_LINEAR)
+    plt.imshow(cv2.cvtColor(im_resized, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+class DCT():    
+    def __init__(self): # Constructor
+        self.message = None
+        self.bitMess = None
+        self.oriCol = 0
+        self.oriRow = 0
+        self.numBits = 0   
+    #encoding part : 
+    def encode_image(self,img,secret_msg):
+        #show(img)
+        secret=secret_msg
+        self.message = str(len(secret))+'*'+secret
+        self.bitMess = self.toBits()
+        #get size of image in pixels
+        row,col = img.shape[:2]
+        ##col, row = img.size
+        self.oriRow, self.oriCol = row, col  
+        if((col/8)*(row/8)<len(secret)):
+            print("Error: Message too large to encode in image")
+            return False
+        #make divisible by 8x8
+        if row%8 != 0 or col%8 != 0:
+            img = self.addPadd(img, row, col)
+        
+        row,col = img.shape[:2]
+        ##col, row = img.size
+        #split image into RGB channels
+        bImg,gImg,rImg = cv2.split(img)
+        #message to be hid in blue channel so converted to type float32 for dct function
+        bImg = np.float32(bImg)
+        #break into 8x8 blocks
+        imgBlocks = [np.round(bImg[j:j+8, i:i+8]-128) for (j,i) in itertools.product(range(0,row,8),range(0,col,8))]
+        #Blocks are run through DCT function
+        dctBlocks = [np.round(cv2.dct(img_Block)) for img_Block in imgBlocks]
+        #blocks then run through quantization table
+        quantizedDCT = [np.round(dct_Block/quant) for dct_Block in dctBlocks]
+        #set LSB in DC value corresponding bit of message
+        messIndex = 0
+        letterIndex = 0
+        for quantizedBlock in quantizedDCT:
+            #find LSB in DC coeff and replace with message bit
+            DC = quantizedBlock[0][0]
+            DC = np.uint8(DC)
+            DC = np.unpackbits(DC)
+            DC[7] = self.bitMess[messIndex][letterIndex]
+            DC = np.packbits(DC)
+            DC = np.float32(DC)
+            DC= DC-255
+            quantizedBlock[0][0] = DC
+            letterIndex = letterIndex+1
+            if letterIndex == 8:
+                letterIndex = 0
+                messIndex = messIndex + 1
+                if messIndex == len(self.message):
+                    break
+        #blocks run inversely through quantization table
+        sImgBlocks = [quantizedBlock *quant+128 for quantizedBlock in quantizedDCT]
+        #blocks run through inverse DCT
+        #sImgBlocks = [cv2.idct(B)+128 for B in quantizedDCT]
+        #puts the new image back together
+        sImg=[]
+        for chunkRowBlocks in self.chunks(sImgBlocks, col/8):
+            for rowBlockNum in range(8):
+                for block in chunkRowBlocks:
+                    sImg.extend(block[rowBlockNum])
+        sImg = np.array(sImg).reshape(row, col)
+        #converted from type float32
+        sImg = np.uint8(sImg)
+        #show(sImg)
+        sImg = cv2.merge((sImg,gImg,rImg))
+        return sImg
+
+    #decoding part :
+    def decode_image(self,img):
+        row,col = img.shape[:2]
+        messSize = None
+        messageBits = []
+        buff = 0
+        #split image into RGB channels
+        bImg,gImg,rImg = cv2.split(img)
+         #message hid in blue channel so converted to type float32 for dct function
+        bImg = np.float32(bImg)
+        #break into 8x8 blocks
+        imgBlocks = [bImg[j:j+8, i:i+8]-128 for (j,i) in itertools.product(range(0,row,8),
+                                                                       range(0,col,8))]    
+        #blocks run through quantization table
+        #quantizedDCT = [dct_Block/ (quant) for dct_Block in dctBlocks]
+        quantizedDCT = [img_Block/quant for img_Block in imgBlocks]
+        i=0
+        #message extracted from LSB of DC coeff
+        for quantizedBlock in quantizedDCT:
+            DC = quantizedBlock[0][0]
+            DC = np.uint8(DC)
+            DC = np.unpackbits(DC)
+            if DC[7] == 1:
+                buff+=(0 & 1) << (7-i)
+            elif DC[7] == 0:
+                buff+=(1&1) << (7-i)
+            i=1+i
+            if i == 8:
+                messageBits.append(chr(buff))
+                buff = 0
+                i =0
+                if messageBits[-1] == '*' and messSize is None:
+                    try:
+                        messSize = int(''.join(messageBits[:-1]))
+                    except:
+                        pass
+            if len(messageBits) - len(str(messSize)) - 1 == messSize:
+                return ''.join(messageBits)[len(str(messSize))+1:]
+        #blocks run inversely through quantization table
+        sImgBlocks = [quantizedBlock *quant+128 for quantizedBlock in quantizedDCT]
+        #blocks run through inverse DCT
+        #sImgBlocks = [cv2.idct(B)+128 for B in quantizedDCT]
+        #puts the new image back together
+        sImg=[]
+        for chunkRowBlocks in self.chunks(sImgBlocks, col/8):
+            for rowBlockNum in range(8):
+                for block in chunkRowBlocks:
+                    sImg.extend(block[rowBlockNum])
+        sImg = np.array(sImg).reshape(row, col)
+        #converted from type float32
+        sImg = np.uint8(sImg)
+        sImg = cv2.merge((sImg,gImg,rImg))
+        ##sImg.save(img)
+        #dct_decoded_image_file = "dct_" + original_image_file
+        #cv2.imwrite(dct_decoded_image_file,sImg)
+        return ''
+      
+    """Helper function to 'stitch' new image back together"""
+    def chunks(self, l, n):
+        m = int(n)
+        for i in range(0, len(l), m):
+            yield l[i:i + m]
+    def addPadd(self,img, row, col):
+        img = cv2.resize(img,(col+(8-col%8),row+(8-row%8)))    
+        return img
+    def toBits(self):
+        bits = []
+        for char in self.message:
+            binval = bin(ord(char))[2:].rjust(8,'0')
+            bits.append(binval)
+        self.numBits = bin(len(bits))[2:].rjust(8,'0')
+        return bits
+
+class LSB():
+    #encoding part :
+    def encode_image(self,img, msg):
+        length = len(msg)
+        if length > 255:
+            print("text too long! (don't exeed 255 characters)")
+            return False
+        encoded = img.copy()
+        width, height = img.size
+        index = 0
+        for row in range(height):
+            for col in range(width):
+                if img.mode != 'RGB':
+                    r, g, b ,a = img.getpixel((col, row))
+                elif img.mode == 'RGB':
+                    r, g, b = img.getpixel((col, row))
+                # first value is length of msg
+                if row == 0 and col == 0 and index < length:
+                    asc = length
+                elif index <= length:
+                    c = msg[index -1]
+                    asc = ord(c)
+                else:
+                    asc = b
+                encoded.putpixel((col, row), (r, g , asc))
+                index += 1
+        return encoded
+
+    #decoding part :
+    def decode_image(self,img):
+        width, height = img.size
+        msg = ""
+        index = 0
+        for row in range(height):
+            for col in range(width):
+                if img.mode != 'RGB':
+                    r, g, b ,a = img.getpixel((col, row))
+                elif img.mode == 'RGB':
+                    r, g, b = img.getpixel((col, row))  
+                # first pixel r value is length of message
+                if row == 0 and col == 0:
+                    length = b
+                elif index <= length:
+                    msg += chr(b)
+                index += 1
+        lsb_decoded_image_file = "lsb_" + original_image_file
+        #img.save(lsb_decoded_image_file)
+        ##print("Decoded image was saved!")
+        return msg
+
+class Compare():
+    def correlation(self, img1, img2):
+        return signal.correlate2d (img1, img2)
+    def meanSquareError(self, img1, img2):
+        error = np.sum((img1.astype('float') - img2.astype('float')) ** 2)
+        error /= float(img1.shape[0] * img1.shape[1]);
+        return error
+    def psnr(self, img1, img2):
+        mse = self.meanSquareError(img1,img2)
+        if mse == 0:
+            return 100
+        PIXEL_MAX = 255.0
+        return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+
+
+
+#driver part :
+#deleting previous folders :
+if os.path.exists("Encoded_image/"):
+    shutil.rmtree("Encoded_image/")
+if os.path.exists("Decoded_output/"):
+    shutil.rmtree("Decoded_output/")
+if os.path.exists("Comparison_result/"):
+    shutil.rmtree("Comparison_result/")
+#creating new folders :
+os.makedirs("Encoded_image/")
+os.makedirs("Decoded_output/")
+os.makedirs("Comparison_result/")
+original_image_file = ""    # to make the file name global variable
+lsb_encoded_image_file = ""
+dct_encoded_image_file = ""
+
+while True:
+    m = input("To embedd press '1', to extract press '2', to compare press '3',to decrypt message press'4', press any other button to close: ")
+
+    if m == "1":
+        os.chdir("Original_image/")
+        original_image_file = input("Enter the name of the file with extension : ")
+        lsb_img = Image.open(original_image_file)
+        dct_img = cv2.imread(original_image_file, cv2.IMREAD_UNCHANGED)
+        print("Description : ",lsb_img,"\nMode : ", lsb_img.mode)
+
+        plaintext = input("Enter the message you want to hide: ")
+        # kunci enkripsi rc4
+        key = input("Enter key:")
+        #waktu mulai enkripsi pesan rahasia
+        start_encrypt = time.time()
+        # proses enkripsi text
+        secret_msg = encrypt(key, plaintext)
+        #waktu selesai encrypt
+        finish_encrypt = time.time() - start_encrypt
+
+        print("The secret message length is (plaintext): ",len(plaintext))
+        print("The secret message length is (ciphertext): ",len(secret_msg))
+        os.chdir("..")
+        os.chdir("Encoded_image/")
+
+        #LSB
+        #waktu mulai encode / embedd LSB
+        start_embedd_LSB = time.time()
+        #proses encode / embed LSB
+        lsb_img_encoded = LSB().encode_image(lsb_img, secret_msg)
+        #waktu selesai encode / embedd LSB
+        finish_embedd_LSB = time.time() - start_embedd_LSB
+        
+        #DCT
+        #waktu mulai encode / embedd DCT
+        start_embedd_DCT = time.time()
+        #proses encode / embedd DCT
+        dct_img_encoded = DCT().encode_image(dct_img, secret_msg)
+        #waktu selesai encode / embedd DCT
+        finish_embedd_DCT = time.time() - start_embedd_DCT
+        
+        #save image hasil encode/embed
+        lsb_encoded_image_file = "lsb_" + original_image_file
+        lsb_img_encoded.save(lsb_encoded_image_file)
+        dct_encoded_image_file = "dct_" + original_image_file
+        cv2.imwrite(dct_encoded_image_file,dct_img_encoded)
+        print("Encoded images were saved!")
+        os.chdir("..")
+
+        #perbandingan waktu proses embedding
+        print("Waktu proses enkripsi pesan : " + str(finish_encrypt))
+        print("Waktu proses embedding LSB : " + str(finish_embedd_LSB))
+        print("Waktu proses embedding DCT : " + str(finish_embedd_DCT))
+
+    elif m == "2":
+        os.chdir("Encoded_image/")
+        lsb_img = Image.open(lsb_encoded_image_file)
+        dct_img = cv2.imread(dct_encoded_image_file, cv2.IMREAD_UNCHANGED)
+        os.chdir("..") #going back to parent directory
+        os.chdir("Decoded_output/")
+        
+        #LSB
+        #start waktu extract LSB
+        start_extract_LSB = time.time()
+        #proses extract / decode LSB
+        lsb_hidden_text = LSB().decode_image(lsb_img)
+        #finish waktu extract LSB
+        finish_extract_LSB = time.time() - start_extract_LSB
+        
+        #DCT
+        #start waktu extract DCT
+        start_extract_DCT = time.time()
+        #proses extract / decode DCT
+        dct_hidden_text = DCT().decode_image(dct_img)
+        #finish waktu extract DCT
+        finish_extract_DCT = time.time() - start_extract_DCT
+
+        file = open("lsb_hidden_text.txt","w")
+        file.write(lsb_hidden_text) # saving hidden text as text file
+        file.close()
+        file = open("dct_hidden_text.txt","w")
+        file.write(dct_hidden_text) # saving hidden text as text file
+        file.close()
+        print("Hidden texts were saved as text file!")
+        os.chdir("..")
+
+        #perbanding waktu proses extraksi
+        print("Waktu proses ekstraksi LSB : ", str(finish_extract_LSB))
+        print("Waktu proses ekstraksi DCT : ", str(finish_extract_DCT))
+
+    elif m == "3":
+        #comparison calls
+        os.chdir("Original_image/")
+        original = cv2.imread(original_image_file)
+        os.chdir("..")
+        os.chdir("Encoded_image/")
+        lsbEncoded = cv2.imread(lsb_encoded_image_file)
+        dctEncoded = cv2.imread(dct_encoded_image_file)
+        original = cv2.cvtColor(original, cv2.COLOR_BGR2RGB)
+        lsb_encoded_img = cv2.cvtColor(lsbEncoded, cv2.COLOR_BGR2RGB)
+        dct_encoded_img = cv2.cvtColor(dctEncoded, cv2.COLOR_BGR2RGB)
+        os.chdir("..")
+        os.chdir("Comparison_result/")
+
+        book = xlwt.Workbook()
+        sheet1=book.add_sheet("Sheet 1")
+        style_string = "font: bold on , color red; borders: bottom dashed"
+        style = xlwt.easyxf(style_string)
+        sheet1.write(0, 0, "Original vs", style=style)
+        sheet1.write(0, 1, "MSE", style=style)
+        sheet1.write(0, 2, "PSNR", style=style)
+        sheet1.write(1, 0, "LSB")
+        sheet1.write(1, 1, Compare().meanSquareError(original, lsb_encoded_img))
+        sheet1.write(1, 2, Compare().psnr(original, lsb_encoded_img))
+        sheet1.write(2, 0, "DCT")
+        sheet1.write(2, 1, Compare().meanSquareError(original, dct_encoded_img))
+        sheet1.write(2, 2, Compare().psnr(original, dct_encoded_img))
+
+        book.save("Comparison.xls")
+        print("Comparison Results were saved as xls file!")
+        os.chdir("..")
+    elif m == "4":
+        # kunci enkripsi rc4
+        key = input("Enter key:")
+        #waktu mulai dekripsi ciphertext pesan rahasia
+        start_decrypt = time.time()
+        # change ciphertext to string again
+        decrypted = decrypt(key, secret_msg)
+        #waktu selesai dekripsi ciphertext pesan rahasia
+        finish_decrypt = time.time() - start_decrypt
+        
+        print('decrypted:', decrypted)
+        print('Waktu proses dekripsi pesan: ',finish_decrypt)
+
+    else:
+        print("Closed!")
+        break
